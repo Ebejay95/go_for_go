9739f6a5d8daff7a9358287c7faa3554b37fee6e
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-10-15 11:20:19 +0200

    little test projects

---
+++ first_go_programm/go.mod
@@ -0,0 +1,3 @@
+module first_go_programm
+
+go 1.23.2
--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -1,19 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   app.go                                             :+:      :+:    :+:   */
+/*   profit_calculator.go                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/14 16:43:20 by jeberle           #+#    #+#             */
-/*   Updated: 2024/10/15 09:34:58 by jonathanebe      ###   ########.fr       */
+/*   Created: 2024/10/15 10:44:11 by jonathanebe       #+#    #+#             */
+/*   Updated: 2024/10/15 11:06:06 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-    fmt.Print("Hello World!\n")
+func main () {
+	const taxRate float64 = 0.19
+	var amount float64
+	var result float64
+
+	fmt.Print("Amount: ")
+	fmt.Scan(&amount)
+	
+	result = amount * (1 - taxRate)
+	//fmt.Println("Real revenue: ", result)
+	fmt.Printf("Real revenue: %.2f\n", result)
 }
