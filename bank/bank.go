9a7fb282b564ea63ac636c634703b61c585e3328
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-10-15 14:34:48 +0200

    error handling

--- bank/bank.go
+++ bank/bank.go
@@ -6,13 +6,18 @@
 /*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 13:01:23 by jonathanebe       #+#    #+#             */
-/*   Updated: 2024/10/15 14:11:46 by jonathanebe      ###   ########.fr       */
+/*   Updated: 2024/10/15 14:34:13 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 const (
 	D = "\033[0m"
@@ -25,10 +30,32 @@ const (
 	W = "\033[37m"
 )
 
+const balanceFileName = "balance.txt"
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(balanceFileName)
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("Failed convert from balance file")
+	}
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceString := fmt.Sprint(balance)
+	os.WriteFile(balanceFileName, []byte(balanceString), 0644);
+}
+
 func main() {
 
-	accountBalance := 1000.0
-	
+	accountBalance, err := getBalanceFromFile()
+	if err != nil {
+		fmt.Println(R + "", err, D + "");
+	}
 	for {
 		fmt.Println(C + "Welcome to Go Bank!" + D)
 		fmt.Println(C + "What do you want to do?" + D)
@@ -52,6 +79,7 @@ func main() {
 				continue
 			}
 			accountBalance += changeAmount
+			writeBalanceToFile(accountBalance)
 			fmt.Println(G + "Your new balance is", accountBalance, "" + D)
 		} else if choice == 3 {
 			fmt.Print(B + "Your Withdraw: " + D)
@@ -65,6 +93,7 @@ func main() {
 				continue
 			}
 			accountBalance -= changeAmount
+			writeBalanceToFile(accountBalance)
 			fmt.Println(G + "Your new balance is", accountBalance, "" + D)
 		} else {
 			fmt.Println(C + "Goodbye from Go Bank!" + D)
